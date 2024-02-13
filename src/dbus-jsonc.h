5000d563e74bdb67e57ef529413b84cb833a4893
Valentin Geffroy <valentin.geffroy@iot.bzh>
2024-02-13 14:39:54 +0100

    Update from internal git sync
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

---
+++ src/dbus-jsonc.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2015-2020 "IoT.bzh"
+ * Author José Bollo <jose.bollo@iot.bzh>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+struct sd_bus_message;
+struct json_object;
+
+extern int msg2jsonc(struct sd_bus_message *msg, struct json_object **result);
+extern int jsonc2msg(struct sd_bus_message *msg, const char *signature, struct json_object *list);
+
