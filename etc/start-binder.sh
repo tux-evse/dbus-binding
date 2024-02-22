e5cde9650b0c77a42381defa0c0af76fde460602
Valentin Geffroy <valentin.geffroy@iot.bzh>
2024-02-22 11:29:41 +0100

    Binder test script correction
    Scrip test works well:
    ++ dirname ./start-binder.sh
    + DIRNAME=.
    + cd ./..
    ++ pwd
    + CONFDIR=/usr/redpesk/dbus-binding/test/etc
    + DEVTOOL_PORT=1231
    + echo dbus binding config=/usr/redpesk/dbus-binding/test/etc/binder-test-dbus.json port=1231
    dbus binding config=/usr/redpesk/dbus-binding/test/etc/binder-test-dbus.json port=1231
    + afb-binder --port=1231 -v --config=/usr/redpesk/dbus-binding/test/etc/binder-test-dbus.json --binding=/usr/redpesk/dbus-binding/lib/dbus-binding.so
       NOTICE: Browser URL= http://localhost:1231
       NOTICE: Serving rootdir=. uploaddir=.
       NOTICE: API dbus starting...
       NOTICE: API monitor starting...
       NOTICE: Listening interface *:1231
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- etc/start-binder.sh
+++ etc/start-binder.sh
@@ -10,8 +10,9 @@ cd $DIRNAME/..
 CONFDIR=`pwd`/etc
 
 DEVTOOL_PORT=1231
-echo Slac debug mode config=$CONFDIR/*.json port=$DEVTOOL_PORT
+echo dbus binding config=$CONFDIR/binder-test-dbus.json port=$DEVTOOL_PORT
 
-afb-binder --name=afb-dbus --port=$DEVTOOL_PORT -v \
+afb-binder --port=$DEVTOOL_PORT -vvv \
   --config=$CONFDIR/binder-test-dbus.json \
+  --binding=/usr/redpesk/dbus-binding/lib/dbus-binding.so \
   $*
